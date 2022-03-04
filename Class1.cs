edaa5c6bd18ecb6b7393162ced6ea76defc92642
parthgarg977 <67135234+Parthgarg977@users.noreply.github.com>
2022-03-04 21:07:58 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Employee_Attendance
+{
+    public class Class1
+    {
+        public byte attendance()
+        {
+            Random random = new Random();
+            byte test = random.Next(0, 2);
+            return test;
+        }
+    }
+}
