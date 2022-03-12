67bc66be919c7b34b3ea19cf683f64f93f424617
parthgarg977 <67135234+Parthgarg977@users.noreply.github.com>
2022-03-13 02:09:30 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
     internal class class1
     {
-        int Is_Full_Time = 1;
+        int Full_Time = 1;
         int Emp_Rate_Per_Hr = 20;
         int empHrs = 0;
         int empWage = 0;
@@ -17,7 +17,7 @@ using System.Threading.Tasks;
     public void Employee()
         {
             int empCheck = obj.Next(0, 2);
-            if (empCheck == Is_Full_Time)
+            if (empCheck == Full_Time)
             {
                 Console.WriteLine("Employee is Present");
             }
@@ -30,15 +30,15 @@ using System.Threading.Tasks;
     public void EmpDailyWages()
         {
             int empCheck = obj.Next(0, 2);
-            if (empCheck == Is_Full_Time)
+            if (empCheck == Full_Time)
             {
                 empHrs = 8;
-                Console.WriteLine("He worked full day");
+                Console.WriteLine("Employee worked full day");
             }
             else
             {
                 empHrs = 0;
-                Console.WriteLine("He is Absent");
+                Console.WriteLine("Employee is Absent");
             }
             empWage = empHrs * Emp_Rate_Per_Hr;
             Console.WriteLine("Employee salary is= " + empWage);
